637424ce69f23beb77d26b69b8b6e8baaae14261
Jeremy Chiaramonte <jeremychiaramonte@gmail.com>
2018-02-09 16:20:57 -0500

    testing remote

--- seleniumTest.py
+++ seleniumTest.py
@@ -1,4 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 browser = webdriver.Firefox()
-browser.get("http://www.supremenewyork.com/shop")
+
+browser.get("https://shop-usa.palaceskateboards.com/")
+#assert 'Google' in browser.title
+
+elem = browser.find_element_by_class_name('product-link')  # Find the search box
+elem.click()
+
+buy = browser.find_element_by_name('button')
+buy.click()
+#elem.send_keys('baik19@vt.edu')
+
+#search = 
