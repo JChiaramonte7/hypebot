c889f82d0356776ba662c78ae26c39bbdf8e5a33
DangTimmyTD <DangTimmyTD@gmail.com>
2018-02-09 17:05:32 -0500

    Started footlocker bot

--- footlocker/footlockerbot.py
+++ footlocker/footlockerbot.py
@@ -5,13 +5,4 @@ from selenium.webdriver.common.keys import Keys
 browser = webdriver.Firefox()
 
 browser.get("https://footlocker.com")
-#assert 'Google' in browser.title
-
-elem = browser.find_element_by_class_name('product-link')  # Find the search box
-elem.click()
-
-buy = browser.find_element_by_name('button')
-buy.click()
-#elem.send_keys('baik19@vt.edu')
-
-#search =
+assert 'Sneakers - Athletic Shoes | Foot Locker' in browser.title
